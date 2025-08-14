907fe2c74ab88e46e301d2b5697994c16ecd670a
Denis Kudelin <15978569+denis-kudelin@users.noreply.github.com>
2025-08-14 04:01:08 +0300

    Inline command list and improve power estimation

--- nut_webgui_upsmc/src/inst_cmd.rs
+++ nut_webgui_upsmc/src/inst_cmd.rs
@@ -2,7 +2,7 @@ use crate::CmdName;
 #[cfg(feature = "serde")]
 use serde::Serialize;
 #[cfg_attr(feature = "serde", derive(Serialize))]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct InstCmd {
   pub id: CmdName,
   pub desc: String,
